3476d0919129742f29d2c6038130a894c532decb
jarjosqua <78829407+jarjosqua@users.noreply.github.com>
2021-04-14 10:25:46 -0500

    tarea en casa

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Hola");
+var menuToggle = document.querySelector("menu-toggle");
+var menu = document.getElementsBylu("menu");
+
+menuToggle.addEventListener("click", (e) => {
+    menu.classlist.toggle("mostrar")
+});
